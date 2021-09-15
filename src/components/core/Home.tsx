6570dcbc761547c9ef874a6b014f8366a6515a68
BLEACH <3027421343@qq.com>
2021-09-15 15:24:14 +0800

    router store add

--- src/components/core/Home.tsx
+++ src/components/core/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from './Layout'
+import {useSelector} from 'react-redux'
+
 
 const Home = () => {
+    const state = useSelector(state => state)
     return (
         <Layout>
             home
+            {JSON.stringify(state)}
         </Layout>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-
+import {Provider} from 'react-redux'
+import store from './store';
+import { ConnectedRouter } from 'connected-react-router';
+import { history } from './store'
 
 ReactDOM.render(
-  <Routes />,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Routes />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
